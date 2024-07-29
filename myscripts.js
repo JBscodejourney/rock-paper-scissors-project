891950ebefebd49f97a153e8f68e57ba56d1a63b
Justin B <156939399+JBscodejourney@user.noreply.github.com>
2024-07-29 11:46:49 -0400

    Write code to play one round of Rock, Paper, Scissors

--- myscripts.js
+++ myscripts.js
@@ -2,6 +2,9 @@ const rock = "rock";
 const paper = "paper";
 const scissors = "scissors";
 
+let humanScore = 0;
+let computerScore = 0;
+
 function getComputerChoice() {
     let randomNumber = Math.floor(Math.random() * 3)
     if (randomNumber === 0) {
@@ -18,3 +21,27 @@ function getComputerChoice() {
 function getHumanChoice() {
    let humanChoice = prompt("Choose: rock, paper, or scissors");
 }
+
+function playRound(humanChoice, computerChoice) {
+    humanChoice.toLowerCase();
+    if (humanChoice === rock && computerChoice === scissors 
+    || humanChoice === scissors && computerChoice === paper
+    || humanChoice === paper && computerChoice === rock) {
+        return console.log("Congrats. You win!")
+        ++humanScore
+    }
+    else if (humanChoice === rock && computerChoice === paper
+        ||  humanChoice === scissors && computerChoice === rock
+        ||  humanChoice === paper && computerChoice === scissors) {
+        return console.log("Oops! Try again.")
+        ++computerScore
+        }
+    else {
+        return console.log("Tie!")
+    }
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
